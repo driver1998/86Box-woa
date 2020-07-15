1bd10adbe759c0c96b6d09d28ffc5fcae6b553b4
GH Cao <driver1998@foxmail.com>
2020-07-15 14:06:49 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 HOST=$ARCH-w64-mingw32
-PREFIX=$PWD/$HOST
 
-export MINGW_ROOT=$PWD/$(find llvm-mingw* -maxdepth 1 -type d | head -n 1)
+sudo mv $PWD/$(find llvm-mingw* -maxdepth 1 -type d | head -n 1) /opt/llvm-mingw
+export MINGW_ROOT=/opt/llvm-mingw
+export PREFIX=$MINGW_ROOT/$HOST
 export PATH=$PATH:$MINGW_ROOT/bin:$PREFIX/bin
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
 
@@ -27,7 +28,7 @@ function build_lib() {
             sed '/^$/d' | sed "/^$DLLFILE/d" | sed '/\.idata/d' | sed '/__imp/d' | sed '/__NULL_IMPORT/d' |
             sed '/_NULL_THUNK_DATA/d' | sed '/__IMPORT_DESCRIPTOR/d' | sed 's/@.*$//g' | sed 's/^_//g'
     ) > $MODULE.def
-    $HOST-dlltool -d $MODULE.def -l $MINGW_ROOT/$HOST/lib/$LIBFILE
+    sudo $HOST-dlltool -d $MODULE.def -l $MINGW_ROOT/$HOST/lib/$LIBFILE
 }
 
 if [ $ARCH == "aarch64" ]
@@ -36,18 +37,18 @@ then
     build_lib "libsetupapi.a"
 fi
 
-mkdir $PREFIX
-mkdir $PREFIX/include
+sudo mkdir $PREFIX
+sudo mkdir $PREFIX/include
 
 # Winpcap headers
 pushd WpdPack/Include
-cp -R * $PREFIX/include
+sudo cp -R * $PREFIX/include
 popd
 
 # zlib
 pushd $(find zlib-* -maxdepth 1 -type d | head -n 1)
 make -j $(nproc) -f win32/Makefile.gcc PREFIX=$HOST- || exit 1
-make -f win32/Makefile.gcc install SHARED_MODE=1 \
+sudo make -f win32/Makefile.gcc install SHARED_MODE=1 \
      BINARY_PATH=$PREFIX/bin \
      INCLUDE_PATH=$PREFIX/include \
      LIBRARY_PATH=$PREFIX/lib || exit 1
@@ -56,10 +57,10 @@ popd
 # libpng
 pushd $(find libpng-* -maxdepth 1 -type d | head -n 1)
 ./configure --prefix=$PREFIX --host=$HOST --disable-static --enable-shared || exit 1
-make -j $(nproc) || exit 1
-make install     || exit 1
+make -j $(nproc)   || exit 1
+sudo make install  || exit 1
 pushd $PREFIX/lib
-ln -s libpng.dll.a libpng.a
+sudo ln -s libpng.dll.a libpng.a
 popd
 popd
 
@@ -72,10 +73,10 @@ autoconf
             --disable-video-opengles --disable-video-opengles1 \
             --disable-video-opengles2 --disable-video-vulkan \
             --disable-static --enable-shared || exit 1
-make -j $(nproc) || exit 1
-make install     || exit 1
+make -j $(nproc)  || exit 1
+sudo make install || exit 1
 pushd $PREFIX/lib
-ln -s libSDL2.dll.a libSDL2.a
+sudo ln -s libSDL2.dll.a libSDL2.a
 popd
 popd
 
@@ -86,18 +87,18 @@ sed -i "s/\/usr\/\${HOST}/$(echo $PREFIX | sed 's/\//\\\//g')/g" XCompile.txt
 cmake . -DCMAKE_TOOLCHAIN_FILE=XCompile.txt -DHOST=$HOST \
         -DDSOUND_LIBRARY=$MINGW_ROOT/$HOST/lib \
         -DDSOUND_INCLUDE_DIR=$MINGW_ROOT/$HOST/include || exit 1
-make -j $(nproc) || exit 1
-make install     || exit 1
+make -j $(nproc)   || exit 1
+sudo make install  || exit 1
 pushd $PREFIX/lib
-ln -s libOpenAL32.dll.a libopenal.a
+sudo ln -s libOpenAL32.dll.a libopenal.a
 popd
 popd
 
 # freetype
 pushd $(find freetype-* -maxdepth 1 -type d | head -n 1)
 ./configure --prefix=$PREFIX --host=$HOST --disable-static --enable-shared || exit 1
-make -j $(nproc) || exit 1
-make install     || exit 1
+make -j $(nproc)   || exit 1
+sudo make install  || exit 1
 popd
 
 # 86Box
@@ -112,9 +113,8 @@ cd src
 if [ $ARCH == "x86_64"  ]; then PLAT='X64=y'   ; fi
 if [ $ARCH == "armv7"   ]; then PLAT="ARM=y"   ; fi
 if [ $ARCH == "aarch64" ]; then PLAT="ARM64=y" ; fi
-ARGS="$PLAT DINPUT=n WINDRES=$HOST-windres STRIP=$HOST-strip"
-make -f win/Makefile_ndr.mingw $ARGS -j $(nproc) || exit 1
-cp 86Box.exe pcap_if.exe $PREFIX/bin
+ARGS="$PLAT DINPUT=n WINDRES=$HOST-windres STRIP=$HOST-strip NEW_DYNAREC=y"
+make -f win/Makefile.mingw $ARGS -j $(nproc) || exit 1
 popd
 
 BIN="86box/src/86Box.exe                \
